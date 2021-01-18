f0822466008a2bdc196bf61c4b1e98c415b4906e
Kevin Leung <kleung@Kevins-MacBook-Pro.local>
2021-01-17 17:50:34 -0700

    update app.vue w/ home icon and proper method

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,16 @@
       >
         Community Mandarin List
       </v-toolbar-title>
+      <v-spacer />
+      <v-icon
+        data-testid="navbar-home-btn"
+        class="navbar-home-btn"
+        color="white"
+        large
+        @click="handleReturnHome"
+      >
+        home
+      </v-icon>
     </v-toolbar>
     <transition appear name="slide-x-transition" mode="out-in">
       <router-view />
@@ -15,11 +25,17 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import router from "@/router";
 
 @Component({
   name: "App"
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public handleReturnHome(): void {
+    router.push({name: "Home"});
+  }
+
+}
 </script>
 
 <style>
